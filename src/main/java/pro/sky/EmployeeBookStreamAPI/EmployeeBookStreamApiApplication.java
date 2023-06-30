f20a1d206b545ea3f0d7c897ce72b7e2749d2eaa
Unteryana <ishtar_v@bk.ru>
2023-06-30 13:32:44 +0300

    initial commit

---
+++ src/main/java/pro/sky/EmployeeBookStreamAPI/EmployeeBookStreamApiApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.EmployeeBookStreamAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookStreamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookStreamApiApplication.class, args);
+	}
+
+}
